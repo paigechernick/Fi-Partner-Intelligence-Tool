8b7967a2515056dca1d15a46d4bcabb4fe551344
paigechernick <paige.chernick@gmail.com>
2026-05-12 15:32:14 -0400

    Add Home component with FiPartnerBrief

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FiPartnerBrief from '@/components/FiPartnerBrief';
+
+export default function Home() {
+  return (
+    <main>
+      <FiPartnerBrief />
+    </main>
+  );
+}
